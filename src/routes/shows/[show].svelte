9d9590f91cb1a29d3563f05085d73fc0f9ce146c
Johannes Gerold <johannes.gerold.ext@gmail.com>
2019-12-02 12:57:17 +0100

    dynamic routes for shows

---
+++ src/routes/shows/[show].svelte
@@ -0,0 +1,19 @@
+<script>
+  import { stores } from '@sapper/app';
+  const { page } = stores();
+
+  let show = "";
+  page.subscribe(val => {
+    show = val.params.show;
+  })
+</script>
+
+<style>
+  div {
+    color: var(--on-primary);
+  }
+</style>
+
+<div>
+  <span>{show}</span>
+</div>
---
+++ src/routes/shows/_layout.svelte
@@ -0,0 +1,20 @@
+<script>
+  // export let segment;
+</script>
+
+<style>
+  ul {
+    color: var(--on-primary);
+  }
+</style>
+
+<div>
+  <ul>
+    <li><a href="shows/invisa">Invisa</a></li>
+    <li><a href="shows/fortuna">Fortuna</a></li>
+    <li><a href="shows/wikinger">Wikinger</a></li>
+    <li><a href="shows/stage">Bühnenauftritte</a></li>
+  </ul>
+</div>
+
+<slot></slot>
